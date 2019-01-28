54a1775581285fdfa7fd91dbde41c2c14f3651d0
Kunal Sheth <kunal@kunalsheth.info>
2019-01-28 12:12:00 -0800

    Disable interrupts when copying tick data

--- ticks-to-serial.ino
+++ ticks-to-serial.ino
@@ -1,3 +1,10 @@
+#define STR_MANUFACTURER L"Funky Monkeys"
+#define STR_PRODUCT L"Ticks to Serial"
+#define STR_SERIAL_NUMBER L""
+
+// incremented by 111
+#define PRODUCT_ID 0x0594
+
 extern "C" {
 #include "protocol.h"
 #include "encoders.h"
@@ -13,19 +20,20 @@ void setup() { // called by arduino main
 ticks_t last_left_ticks = 0;
 ticks_t last_right_ticks = 0;
 
+ticks_t left_copy, right_copy;
+int8_t dleft, dright;
 void loop() { // called by arduino main
-  ticks_t left_copy = get_left_ticks();
-  ticks_t right_copy = get_right_ticks();
+  cli();
+  left_copy = get_left_ticks();
+  right_copy = get_right_ticks();
+  sei();
 
-  int8_t dleft = left_copy - last_left_ticks;
-  int8_t dright = right_copy - last_right_ticks;
+  dleft = left_copy - last_left_ticks;
+  dright = right_copy - last_right_ticks;
 
   last_left_ticks = left_copy;
   last_right_ticks = right_copy;
 
-  if (dleft == 0 && dright == 0) {
-    Serial.send_now();
-  } else {
-    Serial.write(encode(dleft, dright));
-  }
+  if (dleft == 0 && dright == 0) Serial.send_now();
+  else Serial.write(encode(dleft, dright));
 }
